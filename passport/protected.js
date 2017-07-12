ffb06274462d8cceae4dc4711857333f14ca7c99
Michael Frain <michael.frain@hudl.com>
2017-07-12 00:26:56 -0500

    Changed to basic authorization due to a bug with mounted routes

--- passport/protected.js
+++ passport/protected.js
@@ -1,17 +1,20 @@
-var DigestStrategy = require('passport-http').DigestStrategy;
+var BasicStrategy = require('passport-http').BasicStrategy;
+var User = require('../models/user');
+var bCrypt = require('bcrypt-nodejs');
 
 module.exports = function(passport) {
-    passport.use(new DigestStrategy({ qop: 'auth' },
-				    function(username, done) {
-					User.findOne({ username: username }, function (err, user) {
-					    if (err) { return done(err); }
-					    if (!user) { return done(null, false); }
-					    return done(null, user, user.password);
-					});
-				    },
-				    function(params, done) {
-					// validate nonces as necessary
-					done(null, true)
-				    })
-		);
+    passport.use(new BasicStrategy(
+	function(username, password, done) {
+	    User.findOne({ username: username }, function(err, user) {
+		if (err) { return done(err); }
+		if (!user) { return done(null, false); }
+		if (!isValidPassword(user, password)) { return done(null, false); }
+		return done(null, user);
+	    });
+	}
+    ));
+
+    var isValidPassword = function(user, password) {
+	return bCrypt.compareSync(password, user.password);
+    }
 }
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(passport) {
-    router.get('/', passport.authenticate('digest', { session: false }),
+    router.get('/', passport.authenticate('basic', { session: false }),
 	       function(req, res) {
 		   res.render('home', { message: req.flash('message') });
 	       });
