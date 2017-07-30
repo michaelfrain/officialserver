f6aba6670218fc1070d19f164d42eeffaa29253f
Michael Frain <michael.frain@hudl.com>
2017-07-30 16:43:47 -0700

    Functionality for edit members page

--- public/js/editmembers.js
+++ public/js/editmembers.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
         url: '/api/editmembers'
     }).then(function(data) {
         for(var i=0; i < data.length; i++) {
-            $('#members').append('<option value='+data[i].id+'>'+data[i].firstName+'&nbsp;'+data[i].lastName+'</option>');
+            var newValue = i + 1;
+            $('#members').append('<option value='+newValue+'>'+data[i].firstName+'&nbsp;'+data[i].lastName+'</option>');
         }
     });
 });
--- public/js/selection.js
+++ public/js/selection.js
@@ -1,4 +1,18 @@
-$( "select" )
-    .change(function() {
-    $( "form-editmembers-hidden" ).css( "display" : "initial" );
+$(document).on('change', 'select', function() {
+    var selectedOption = $('#members').val();
+    if (selectedOption == 0) {
+        $('form.form-editmembers-hidden').css('display','none');
+    } else {
+        $.ajax({
+            url: 'api/editmembers'
+        }).then(function(data) {
+            var changeUser = data[selectedOption - 1]
+            $('#inputFirstName').val(changeUser.firstName);
+            $('#inputLastName').val(changeUser.lastName);
+            $('#inputEmail').val(changeUser.username);
+            $('#roleEdit').val(changeUser.role);
+            $('#editmember').attr('action', 'api/editmember/' + changeUser._id);
+            $('form.form-editmembers-hidden').css('display','block');
+        });
+    }
 });
